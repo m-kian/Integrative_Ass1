222e09bc1ce7ea1d7e441184e4d7cddcc0bb9a22
m-kian <kheannemiguel15@gmail.com>
2026-02-10 23:07:43 +0800

    .

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -1,18 +1,63 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
     public function index()
     {
-        return response()->json([
-            'users' => [
-                ['id' => 1, 'name' => 'Kheanne'],
-                ['id' => 2, 'name' => 'Micheal'],
-            ]
+        $users = User::all();
+        return response()->json(['data' => $users], 200);
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:6',
         ]);
+
+        $user = User::create($validated);
+
+        return response()->json(['data' => $user], 201);
+    }
+    public function show(string $id)
+    {
+        $user = User::findOrFail($id);
+
+        return response()->json(['data' => $user], 200);
+    }
+
+    public function update(Request $request, string $id)
+    {
+        $user = User::findOrFail($id);
+
+        $validated = $request->validate([
+            'name' => 'sometimes|string|max:255',
+            'email' => [
+                'sometimes',
+                'email',
+                Rule::unique('users', 'email')->ignore($user->id),
+            ],
+            'password' => 'sometimes|string|min:6',
+        ]);
+
+        $user->update($validated);
+
+        return response()->json(['data' => $user], 200);
+    }
+
+
+    public function destroy(string $id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+
+        return response()->json(null, 204);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,20 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\UserController;
 
-Route::get('/users', [UserController::class, 'index']);
+Route::get('/user', function (Request $request) {
+    return response()->json(['message' => 'Hello World!']);
+});
+
+Route::controller(UserController::class)
+    ->prefix('users')
+    ->name('users.')
+    ->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::post('/', 'store')->name('store');
+        Route::get('/{id}', 'show')->name('show');
+        Route::put('/{id}', 'update')->name('update');
+        Route::delete('/{id}', 'destroy')->name('destroy');
+    }); 
