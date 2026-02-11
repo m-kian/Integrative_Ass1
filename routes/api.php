37cd7f9af316b79bdaebc5a1554fb1575705e6d3
m-kian <kheannemiguel15@gmail.com>
2026-02-11 08:51:01 +0800

    commit

--- routes/api.php
+++ routes/api.php
@@ -2,19 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
+use App\Http\controllers\UserController;
 
-Route::get('/user', function (Request $request) {
-    return response()->json(['message' => 'Hello World!']);
+Route::prefix('users')->group(function(){
+    Route::get('/', [UserController::class, 'index']);
+    Route::post('/', [UserController::class, 'store']);
+    Route::get('/{id}', [UserController::class, 'show']);
+    Route::put('/{id}', [UserController::class, 'update']);
+    Route::delete('/{id}', [UserController::class, 'destroy']); 
 });
-
-Route::controller(UserController::class)
-    ->prefix('users')
-    ->name('users.')
-    ->group(function () {
-        Route::get('/', 'index')->name('index');
-        Route::post('/', 'store')->name('store');
-        Route::get('/{id}', 'show')->name('show');
-        Route::put('/{id}', 'update')->name('update');
-        Route::delete('/{id}', 'destroy')->name('destroy');
-    }); 
